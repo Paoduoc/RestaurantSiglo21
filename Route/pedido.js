fe924bd6fbda65c32eaa721a569a57a804173b0b
Paoduoc <pao.arcos@duocuc.cl>
2022-11-19 18:55:26 -0300

    Endpoint caja con validaciones sin pruebas, validaciones bodega, pedido

--- Route/pedido.js
+++ Route/pedido.js
@@ -6,7 +6,9 @@ const Pedidos = require('../Controller/pedido');
 const { garzonValidador } = require('../helpers/validadorBD');
 const pedido = new Pedidos();
 
-router.get('/',( req , res ) =>{ pedido.getAllPedido( req, res ) });
+router.get('/',[
+    check('fechaIP','La fecha es requerida').not().isEmpty(),
+],( req , res ) =>{ pedido.getAllPedido( req, res ) });
 
 router.get('/:id',[
     check('id','no es un id mongodb').isMongoId(),
@@ -15,8 +17,11 @@ router.get('/:id',[
 
 router.post('/',[
     check('garzon','El garzón es requerido').not().isEmpty(),
+    check('garzon','no es un id mongodb').isMongoId(),
     check('reserva','La mesa es requerida').not().isEmpty(),
+    check('reserva','no es un id mongodb').isMongoId(),
     check('platosID','Los platos son requeridos').not().isEmpty(),
+    check('platosID','no es un id mongodb').isMongoId(),
     check('garzon').custom(garzonValidador),
     validadorCampos
     ],( req , res ) =>{ pedido.postPedido( req, res ) });
